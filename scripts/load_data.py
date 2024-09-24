e7b0b64100f3554b632bca75dba1cac4d04267fc
Elisha <elishabu28@gmail.com>
2024-09-24 20:56:23 +0300

    Task 2 started

---
+++ scripts/load_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def load_data(file_path):
+    logging.info("Loading data from file in notebooks dir")
+    try:
+        df = pd.read_csv(file_path)
+        logging.info(f"Data loaded with shape {df.shape}")
+    except Exception as e:
+        logging.error(f"Error loading data: {e}")
+    return df
---
+++ scripts/sales_prediction.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def load_data(file_path):
+    logging.info("Loading data from file in notebooks dir")
+    try:
+        df = pd.read_csv(file_path)
+        logging.info(f"Data loaded with shape {df.shape}")
+    except Exception as e:
+        logging.error(f"Error loading data: {e}")
+    return df
